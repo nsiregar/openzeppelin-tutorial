90c76930e02e0d97d99a3c25847cf9c9e03c61ab
Ngalim Siregar <ngalim.siregar@gmail.com>
2022-01-22 12:17:44 +0700

    Use test env for testing contract

--- test/Box.test.js
+++ test/Box.test.js
@@ -1,29 +1,31 @@
+const { accounts, contract } = require('@openzeppelin/test-environment');
 const { expect } = require('chai');
 const { BN, expectEvent, expectRevert} = require('@openzeppelin/test-helpers');
-const Box = artifacts.require('Box');
+const Box = contract.fromArtifact('Box');
 
-contract('Box', function ([ owner, other]) {
+describe('Box', function () {
+    const [ owner, other ] = accounts;
     const value = new BN('42');
 
     beforeEach(async function () {
-        this.box = await Box.new({from: owner});
+        this.contract = await Box.new({from: owner});
     });
     
     it('retrieve returns value previously stored', async function () {
-        await this.box.store(value, { from: owner });
+        await this.contract.store(value, { from: owner });
 
-        expect(await this.box.retrieve()).to.be.bignumber.equal(value);
+        expect(await this.contract.retrieve()).to.be.bignumber.equal(value);
     })
     
     it('store emits an event', async function () {
-        const receipt = await this.box.store(value, { from: owner });
+        const receipt = await this.contract.store(value, { from: owner });
 
         expectEvent(receipt, 'ValueChanged', {value: value});
     })
     
     it('non owner cannot store a value', async function () {
         await expectRevert(
-            this.box.store(value, { from: other }),
+            this.contract.store(value, { from: other }),
             'Ownable: caller is not the owner',
         )
     })
