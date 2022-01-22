e854319dbb650303af5de1196b8e6b7cad1b1cfd
Ngalim Siregar <ngalim.siregar@gmail.com>
2022-01-22 11:34:29 +0700

    Create basic smart contract with auth

---
+++ contracts/Box.sol
@@ -0,0 +1,27 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./access-control/Auth.sol";
+
+contract Box {
+    uint256 private _value;
+    Auth private _auth;
+    
+    constructor() {
+        _auth = new Auth(msg.sender);
+    }
+
+    event ValueChanged(uint256 value);
+
+    function store(uint256 value) public {
+        require(_auth.isAdministrator(msg.sender), "Unauthorized");
+
+        _value = value;
+        emit ValueChanged(value);
+    }
+    
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
